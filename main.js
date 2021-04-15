cbdf89cd8d75e7630712061accd1472dd51d8357
rinuma-dev <rinumadev.mail@gmail.com>
2021-04-15 09:33:00 +0700

    xhr to fetch

--- main.js
+++ main.js
@@ -1,36 +1,32 @@
 const APIkey = '4c0a0fe804be477191cfc6e99d889f28';
-const Base = "https://api.spoonacular.com/recipes/complexSearch";
+const BaseUrl = "https://api.spoonacular.com/recipes/complexSearch";
 
-const getRecipes =(queries)=>{
-  const xhr = new XMLHttpRequest();
-
-  xhr.onload = function(){
-    const responseJson = JSON.parse(this.responseText);
-    console.log('responseJson',responseJson);
-    if (responseJson.totalResults = 0){
+const getRecipes = (queries) =>{
+  fetch(`${BaseUrl}${queries}&apiKey=${APIkey}`)
+  
+  .then(response => { 
+    return response.json();
+  })
+  .then(responseJson => {
+    
+    if (responseJson.totalResults == 0) {
       showResponseMessage(responseJson.message);
     } else {
-        renderAllRecipes(responseJson.results);
+      renderAllRecipes(responseJson.results);
     }
-  }
+  })
+  .catch(error=>{
+    showResponseMessage(error);
+  })
+}
 
-  xhr.onerror = function(){
-    showResponseMessage();
-  }
 
-  xhr.open("GET",`${Base}?query=${queries}&apiKey=${APIkey}`);
-  // xhr.setRequestHeader()
-  console.log(`${Base}${queries}`);
 
-  xhr.send();
-};
-
- 
 const renderAllRecipes = (results) => {
   const listRecipes = document.querySelector(".results");
   listRecipes.innerHTML = " ";
 
-  results.forEach(result=>{
+  results.forEach(result => {
     listRecipes.innerHTML += `
     <div>
     ${result.id}
@@ -44,101 +40,93 @@ const renderAllRecipes = (results) => {
   });
 }
 
-const showResponseMessage = (message = "Check your connection")=>{
+const showResponseMessage = (message = "Check your connection") => {
   alert(message);
+  console.log(message);
 }
 
 
-document.addEventListener("DOMContentLoaded",() => {
-const buttonSearch =document.querySelector('#search-button');
-const buttonFilter =  document.querySelector('#filter-button');
-const filterSide = document.getElementById("filter");
+document.addEventListener("DOMContentLoaded", () => {
+  const buttonSearch = document.querySelector('#search-button');
+  const buttonFilter = document.querySelector('#filter-button');
+  const filterSide = document.getElementById("filter");
 
-const cuisineCheckBoxes = document.querySelectorAll('input[type="checkbox"][name=cuisine]')
-const intoleranceCheckBoxes = document.querySelectorAll('input[type="checkbox"][name=dairy]')
-const dietCheckBoxes = document.querySelectorAll('input[type="radio"][name=diets]')
+  const cuisineCheckBoxes = document.querySelectorAll('input[type="checkbox"][name=cuisine]')
+  const intoleranceCheckBoxes = document.querySelectorAll('input[type="checkbox"][name=dairy]')
+  const dietCheckBoxes = document.querySelectorAll('input[type="radio"][name=diets]')
 
-const filterChecked ={query:"",diet:"", intolerances:"", cuisine:""};
-let queries ="";
+  const filterChecked = { query: "", diet: "", intolerances: "", cuisine: "" };
+  let queries = "";
 
 
-const checkedBoxestoArray = (elements)=>{
-  return Array.from(elements).filter(i => i.checked).map(i => i.value);
-}
-
-buttonSearch.addEventListener("click",()=>{
-  const searchInput = document.getElementById("search-input").value;
-  if (searchInput!== null || 'undefined'){
-    filterChecked.query = searchInput.toString();
-    queries =constructQuery(filterChecked);
-
-    console.log(queries);
-    getRecipes(queries);
-  }else{ 
-    console.log("please write input");
-    // getRecipes(queries);
+  const checkedBoxestoArray = (elements) => {
+    return Array.from(elements).filter(i => i.checked).map(i => i.value);
   }
 
-  
-})
+  buttonSearch.addEventListener("click", () => {
+    const searchInput = document.getElementById("search-input").value;
+    if (searchInput !== null || 'undefined') {
+      filterChecked.query = searchInput.toString();
+      queries = constructQuery(filterChecked);
 
-buttonFilter.addEventListener("click",()=> {
-  if (filterSide.style.display === "none") { return  filterSide.style.display = "block";} 
-  else { return  filterSide.style.display = "none";}
-});
+      console.log(queries);
+      getRecipes(queries);
+    } else {
+      console.log("please write input");
+      // getRecipes(queries);
+    }
 
 
+  })
 
-cuisineCheckBoxes.forEach(function(checkBox){
-  checkBox.addEventListener('change', function(){
-    filterChecked.cuisine = checkedBoxestoArray(cuisineCheckBoxes).toString();
-    queries = constructQuery(filterChecked);
-  
+  buttonFilter.addEventListener("click", () => {
+    if (filterSide.style.display === "none") { return filterSide.style.display = "block"; }
+    else { return filterSide.style.display = "none"; }
   });
 
-});
 
 
-intoleranceCheckBoxes.forEach(function(checkBox){
-  checkBox.addEventListener('change', function(){
-    filterChecked.intolerances = checkedBoxestoArray(intoleranceCheckBoxes).toString();
-    queries = constructQuery(filterChecked);
-   
+  cuisineCheckBoxes.forEach(function (checkBox) {
+    checkBox.addEventListener('change', function () {
+      filterChecked.cuisine = checkedBoxestoArray(cuisineCheckBoxes).toString();
+      queries = constructQuery(filterChecked);
+
+    });
+
   });
 
-});
 
-dietCheckBoxes.forEach(function(checkBox){
-  checkBox.addEventListener('change', function(){
-  filterChecked.diet = checkedBoxestoArray(dietCheckBoxes).toString();
-  queries = constructQuery(filterChecked);
-  
+  intoleranceCheckBoxes.forEach(function (checkBox) {
+    checkBox.addEventListener('change', function () {
+      filterChecked.intolerances = checkedBoxestoArray(intoleranceCheckBoxes).toString();
+      queries = constructQuery(filterChecked);
+
+    });
+
   });
-});
+
+  dietCheckBoxes.forEach(function (checkBox) {
+    checkBox.addEventListener('change', function () {
+      filterChecked.diet = checkedBoxestoArray(dietCheckBoxes).toString();
+      queries = constructQuery(filterChecked);
+
+    });
+  });
 
 
-  const constructQuery = (filterObject)=>{
+  const constructQuery = (filterObject) => {
     let query = "";
-    Object.keys(filterObject).forEach((property)=>{
-      
-      if(filterObject[property] !== ""){
-        if(property == "query"){
+    Object.keys(filterObject).forEach((property) => {
+
+      if (filterObject[property] !== "") {
+        if (property == "query") {
           query = query.concat(`?${property}=${filterObject[property]}`);
-        }else {
-          query = query.concat(`&${property}=${filterObject[property]}`)}
+        } else {
+          query = query.concat(`&${property}=${filterObject[property]}`)
+        }
       }
     });
     return query
   }
-  
-
-
-
-
-
-
-
-
-
 
 });
